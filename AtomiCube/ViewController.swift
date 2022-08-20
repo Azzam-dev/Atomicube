a0494bf96c10ab3819c0c7595ce8b72ae60bd3aa
Azzam R Alrashed <azzam.rar@gmail.com>
2022-08-20 23:06:39 +0300

    Initial Commit

---
+++ AtomiCube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AtomiCube
+//
+//  Created by Azzam AL-Rashed on 20/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
