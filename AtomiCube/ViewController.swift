995c394f3bfdc5539380df6658f140c8ef2f5aaa
omar0azx <a3mr.azx@gmail.com>
2022-08-22 22:50:09 +0300

    Add constraints for content in Main storyboard, and been linked to ViewController

--- AtomiCube/ViewController.swift
+++ AtomiCube/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sliderMood: UISlider!
+    @IBOutlet weak var imageMood: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let savePosition = UserDefaults.standard.value(forKey: "savePosition") {
+            sliderMood.value = savePosition as! Float
+            sliderChanged(sliderMood)
+        }
+        
     }
-
-
+ 
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        UserDefaults.standard.set(sender.value, forKey: "savePosition")
+        switch sender.value {
+        case 0...25:
+            imageMood.text = "😩"
+        case 26...45:
+            imageMood.text = "😒"
+        case 46...55:
+            imageMood.text = "😐"
+        case 56...75:
+            imageMood.text = "🙂"
+        case 76...100:
+            imageMood.text = "😊"
+        default:
+            print("error")
+        }
+    }
+    
 }
 
