f9780b9502ca27639c49db1e81ddfa73c8ccd067
Azzam R Alrashed <azzam.rar@gmail.com>
2022-08-26 05:33:55 +0300

    Update: @IBAction's for the start buttons on DayRatingVC

--- AtomiCube/MVC/Controllers/DayRatingVC.swift
+++ AtomiCube/MVC/Controllers/DayRatingVC.swift
@@ -18,11 +18,31 @@ class DayRatingVC: UIViewController {
     
     @IBAction func didPreesStare1(_ sender: Any) {
         
-        view.backgroundColor = .orange
+        view.backgroundColor = .red
     }
     
     
+    @IBAction func didPreesStare2(_ sender: Any) {
+        
+        view.backgroundColor = .orange
+    }
     
+    @IBAction func didPreesStare3(_ sender: Any) {
+        
+        view.backgroundColor = .white
+    }
+    
+    
+    @IBAction func didPreesStare4(_ sender: Any) {
+        
+        view.backgroundColor = .yellow
+    }
+    
+    
+    @IBAction func didPreesStare5(_ sender: Any) {
+        
+        view.backgroundColor = .systemYellow
+    }
     
     
     
