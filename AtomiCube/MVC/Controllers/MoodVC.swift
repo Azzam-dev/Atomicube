988b7a945b81e198c893a81375ff40255f66ce62
Azzam R Alrashed <azzam.rar@gmail.com>
2022-09-08 07:11:14 +0300

    Add: MoodService and move all functions related to Mood to it

--- AtomiCube/MVC/Controllers/MoodVC.swift
+++ AtomiCube/MVC/Controllers/MoodVC.swift
@@ -31,41 +31,11 @@ class MoodVC: UIViewController, Storyboarded {
     
     @IBAction func sliderEventDidChange(_ sender: UISlider, forEvent event: UIEvent) {
         let todaysMoodValue = sender.value
-        moodEmojiView = getMoodEmoji(with: todaysMoodValue)
+        MoodService.shared.getMoodEmoji(for: moodEmojiView, with: todaysMoodValue)
         if let eventPhase = event.allTouches?.first?.phase {
             if eventPhase == .ended {
                 mainStore.dispatch(UpdateMoodAction(newMoodValue: todaysMoodValue))
             }
-        }
-    }
-    
-    func lottileAnimation(type: String) -> AnimationView {
-        moodEmojiView.animation = Animation.named(type)
-        moodEmojiView.contentMode = .scaleAspectFit
-        moodEmojiView.play()
-        return moodEmojiView
-    }
-    
-    fileprivate func getMoodEmoji(with moodValue: Float?) -> AnimationView {
-        guard let moodValue = moodValue else {
-            print("getMoodEmoji() received nil value")
-            return lottileAnimation(type: "poker")
-        }
-
-        switch moodValue.rounded() {
-        case 0...25:
-             return lottileAnimation(type: "sad")
-        case 26...45:
-            return lottileAnimation(type: "sulked")
-        case 46...55:
-            return lottileAnimation(type: "poker")
-        case 56...75:
-            return lottileAnimation(type: "smiley")
-        case 76...100:
-            return lottileAnimation(type: "lovelyKiss")
-        default:
-            print("getMoodEmoji() received out of range value")
-            return lottileAnimation(type: "poker")
         }
     }
     
@@ -93,7 +63,7 @@ extension MoodVC: StoreSubscriber {
     
     func newState(state: AppState) {
         sliderMood.value = mood.values.last ?? 50.0
-        moodEmojiView = getMoodEmoji(with: mood.values.last)
+        MoodService.shared.getMoodEmoji(for: moodEmojiView, with: mood.values.last)
         progressMood.progress = getAverageMood(from: mood)
     }
 }
