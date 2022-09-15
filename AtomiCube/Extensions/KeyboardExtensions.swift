282f19fd220b987258563488f978480aa0b26bc5
Azzam R Alrashed <azzam.rar@gmail.com>
2022-09-15 05:56:32 +0300

    Update: refactor Extension files

---
+++ AtomiCube/Extensions/KeyboardExtensions.swift
@@ -0,0 +1,41 @@
+//
+//  KeyboardExtensions.swift
+//  AtomiCube
+//
+//  Created by Azzam AL-Rashed on 15/09/2022.
+//
+
+import UIKit
+
+//this dismiss Keyboard
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+extension UIView {
+    func bindToKeyBoard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)) , name: UIResponder.keyboardWillChangeFrameNotification , object: nil)
+        
+    }
+    
+    @objc func keyboardWillChange(_ notification: NSNotification ) {
+        
+        let duration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+        let beginningFrame = (notification.userInfo![UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let endFrame = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltaY = endFrame.origin.y - beginningFrame.origin.y
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIView.KeyframeAnimationOptions(rawValue: curve), animations: {
+            self.frame.origin.y += deltaY
+        }, completion: nil)
+        
+    }
+}
