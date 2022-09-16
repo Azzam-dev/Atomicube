7de5b65f080b76706b26e6dfe7c391b9acb4899b
omar0azx <a3mr.azx@gmail.com>
2022-09-16 06:02:18 +0300

    Add: initialization for firebase

--- AtomiCube/App/AppDelegate.swift
+++ AtomiCube/App/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 import IQKeyboardManagerSwift
 
 @main
@@ -16,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         RealmRepository.shared.realmConfiguration()
-        
+        FirebaseApp.configure()
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.enableAutoToolbar = false
         
